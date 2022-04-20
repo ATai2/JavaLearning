0a6ae784898e4f517cf9c9d895875d987d7c3877
atai2 <1114677814@qq.com>
2022-04-20 22:36:29 +0800

    尺寸

--- apm-agent/src/main/java/com/atai/apmagent/ATServer.java
+++ apm-agent/src/main/java/com/atai/apmagent/ATServer.java
@@ -1,8 +1,15 @@
 package com.atai.apmagent;
 
+import java.util.concurrent.TimeUnit;
+
 public class ATServer {
     public Integer sayHello(String name, String message) {
         System.out.println("Hello v2");
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return 0;
     }
 
